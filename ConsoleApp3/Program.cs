fd9d22305b9c3af8d7631bb0e7833203063fdab9
Dr.r Dr.r <seg.age.96@mail.ru>
2020-09-22 10:17:35 +0300

    wef32rwfd

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
